4a44af02daff82d0cf6b45e82d6d6219ad077c59
ottone <41752279+ottone@users.noreply.github.com>
2018-11-18 09:00:22 +0100

    Server Web con Thread 18-11-2018

--- webServerThread.py
+++ webServerThread.py
@@ -7,31 +7,45 @@ import _thread
 from socket import *
 import sys          # In order to terminate the program
 
+#************ CONSTANT VARIABLES **************
+BACKLOG = 10 				# How many pending connections queue will hold
+MAX_DATA_RECV = 2048		# Max number of bytes we receive at once
+
+# Assign a port number
+serverPort = 6789
+
 def proxy_thread(conn, client_addr):
 	
 	try:
 		# Get the request from browser
-		request = conn.recv(MAX_DATA_RECV)
+		request = conn.recv(MAX_DATA_RECV).decode()
+		# print request message
+		print('Request Message:', request)
 		# parse the first line
 		filename = request.split()[1]
-		print('filename is:', filename)
-		f = open(filename[1:])
+		print('filename is:', filename[1:])
+		filepath = "C:\\Users\\Francesco\\Documents\\GitHub\\python\\"
+		print('Percorso + Filename :',filepath+filename[1:])
+		f = open(filepath+filename)
+		# Store the entire content of the requested  file in a temporary buffer
 		outputdata = f.read()
+		#Send one HTTP response header line into socket
 		conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-		for i in range (0, len(outputdata)):
+		#Send the content of the requested file to the client
+		for i in range(0, len(outputdata)):
 			conn.send(outputdata[i].encode())
 		conn.send("\r\n".encode())
+		# Close the client connection socket
 		conn.close()
+		exit
 	except IOError:
 		# Send responce message for file not found
 		conn.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
 		conn.send("<html><head>File not found</head><body><h1>File not Found</h1></body></html>".encode())
 		# Close client socket
 		conn.close()
+		exit
 
-#************ CONSTANT VARIABLES **************
-BACKLOG = 10 				# How many pending connections queue will hold
-MAX_DATA_RECV = 999999		# Max number of bytes we receive at once
 
 # Create a TCP  server socket
 # (AF_INET is used for IPv4 protocols)
@@ -39,11 +53,7 @@ MAX_DATA_RECV = 999999		# Max number of bytes we receive at once
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-# Assign a port number
-serverPort = 6789
-
 #print ('hostname is: ', gethostname())
-
 #Prepare a sever socket. Bind the socket to server address and server port 
 serverSocket.bind(('',serverPort))
 
