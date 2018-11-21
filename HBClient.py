8914d6d653d72d86ed2afba120ac193da0a1bbbb
ottone <41752279+ottone@users.noreply.github.com>
2018-11-21 23:00:26 +0100

    UDP HeartBeat

---
+++ HBClient.py
@@ -0,0 +1,27 @@
+""" PYHEARTBEAT CLIENT: SENDS AN UDP PACKET TO A GIVEN SERVER EVERY 10 SECONDS
+    ADJUST THE CONSTANT PARAMENTER AS NEEDED, OR CALL AS: PYHBCLIENT.PY SERVERIP  [UDPPORT]
+"""
+
+from socket import socket
+from time import time, ctime, sleep
+import sys
+
+SERVERIP = '127.0.0.1'		#	LOCAL HOST, JUST FOR TESTING
+HBPORT = 43728				# 	AN ARBITRARY UDP PORT
+BEATWAIT = 10 				# 	NUMBER OF SECONDS BETWEEN HEARTBEATS
+
+if len(sys.argv) > 1:
+	SERVERIP=sys.argv[1]
+if len(sys.argv) > 2:
+	HBPORT=sys.argv[2]
+	
+hbsocket = socket(AF_INET, SOCK_DGRAM)
+print('HBClient sending to IP: ', SERVERIP , 'port :', HBPORT)
+print('\n*** Press Ctrl - C to terminate ***\n')
+
+while 1:
+	hbsocket.sendto('Thump!', (SERVERIP,HBPORT))
+	if __debug__:
+		print('Time: ' , ctime(time()))
+	sleep(BEATWAIT)
+		
