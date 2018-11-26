19d748b504e1a34f9148899fb662acc8063a2eb4
ottone <41752279+ottone@users.noreply.github.com>
2018-11-26 18:00:44 +0100

    pinger

--- SMTPClient_SECURE.py
+++ SMTPClient_SECURE.py
@@ -1,51 +1,40 @@
-# Skeleton Python Code for the Mail Client
+# Skeleton Python Code for the Mail Client 
+# USING STARTTLS
 
 from socket import *
 
 msg = "\r\n I love computer networks!" 
 endmsg = "\r\n.\r\n"
-
-# Choose a mail server (e.g. Google mail server) and call it mailserver mailserver = #Fill in start #Fill in end
-# Create socket called clientSocket and establish a TCP connection with mailserver
-
-mailserver = input("Choose a mail server: ")
-name = input("Inserire il nome utente : ")
-port = 25
+mailserver = 'smtp.gmail.com'
+port = 587
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
-print("Connessione al server : " , mailserver, " porta :  " , port)
-
 clientSocket.connect((mailserver,port))
 recv = clientSocket.recv(1024).decode() 
-
 if recv[:3] != '220':
 	print('220 reply not received from server.')
 else:
 	print("Risposta del Server alla connessione :", recv)
 
 # Send HELO command and print server response. 
-
+input = ("Send an HELO command...")
 heloCommand = 'HELO Alice\r\n'
-print("Saluto server con :", heloCommand)
-
 clientSocket.send(heloCommand.encode())
-
 recv = clientSocket.recv(1024).decode() 
-
 if recv[:3] != '250':
 	print('250 reply not received from server.')
 else:
 	print("Risposta del Server : ", recv)
 	
-	
 # Send MAIL FROM command and print server response.
-
+command = 'STARTTLS\r\n'
+clientSocket.send(command.encode())
+recv = clientSocket.recv(1024).decode()
+print("Risposta del Server al comando STARTTLS: ", recv)
 mailfrom = 'MAIL FROM: <alice@local.com>\r\n'
 print("Invio MAIL FROM al server : ", mailfrom)
 clientSocket.send(mailfrom.encode())
 recv = clientSocket.recv(1024).decode()
-
-print(recv[:3])
 
 if recv[:3] != '250':
 	print('250 reply not received from server.')
