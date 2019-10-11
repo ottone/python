666bf2a0597d78453a3f51032c2b0b4f4bfdfa89
ottone <41752279+ottone@users.noreply.github.com>
2019-10-11 22:07:25 +0200

    prova

--- TCPClient.py
+++ TCPClient.py
@@ -1,10 +1,11 @@
 from socket import *
 serverName = '127.0.0.1'
 serverPort = 12000
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-sentence = input('Frase in minuscolo:')
-clientSocket.send(sentence.encode())
-modifiedSentence, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedSentence.decode())
-clientSocket.close()
+while True:
+  clientSocket = socket(AF_INET, SOCK_STREAM)
+  clientSocket.connect((serverName, serverPort))
+  sentence = input('Frase in minuscolo:')
+  clientSocket.send(sentence.encode())
+  modifiedSentence, serverAddress = clientSocket.recvfrom(2048)
+  print(modifiedSentence.decode())
+  clientSocket.close()
